8c5f27d32884f3a134fd4733ca4dd5e9a60fa14f
Dominic Stephens <chstephens@chapman.edu>
2019-11-08 10:48:44 -0800

    Adds username.sh

---
+++ username.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# username.sh
+# author
+echo "Enter a username"
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[a-z][0-9_a-z]{2,12}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username - lowercase letters and numbers only!"
+	echo "enter a username with lowercase letters and numbers only "
+	read USERNAME
+done
+echo "Thank you"
